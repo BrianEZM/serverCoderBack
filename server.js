f8af94d932efc320403519ffc8c2271f3d5cd617
Brian_Zapata_Marin <brianezm@gmail.com>
2022-06-27 23:17:11 -0300

    subiendo desafio server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080
+
+const server = app.listen(PORT, () => {
+    console.log(`Servidor escuchando al puerto ${server.address().port}`);
+})
+server.on('error', error => console.log(`Error en servidor ${error}`));
+
+app.get('/', (req, res) => {
+    res.send('Hola Brrrr')
+})
+
+app.get('/uno', (req, res) => {
+    res.send('Hola Br 1')
+})
+
+app.get('/dos', (req, res) => {
+    res.send('Hola Br 2')
+})
+
+
